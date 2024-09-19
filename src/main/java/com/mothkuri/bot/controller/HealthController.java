fe70c7116c39d689ab498d5ae4f6d20e4579583f
Sandeep <140805371+saandeepaiml@users.noreply.github.com>
2024-09-19 16:56:56 +0530

    Update HealthController.java

--- src/main/java/com/mothkuri/bot/controller/HealthController.java
+++ src/main/java/com/mothkuri/bot/controller/HealthController.java
@@ -1,18 +1,12 @@
 package com.mothkuri.bot.controller;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Mono;
 
 @RestController
 public class HealthController implements HealthIndicator {
-
-
-    @Value("${bot.custom.message}")
-    public String customMessage;
 
 
     @Override
@@ -23,9 +17,4 @@ public class HealthController implements HealthIndicator {
                 .build();
     }
 
-    @GetMapping("/custom/health")
-    public Mono<Health> customHealth(){
-        Health health = Health.up().status(customMessage).build();
-       return Mono.just(health);
-    }
 }
